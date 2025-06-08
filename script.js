c3f82ecd15d52ef968da10a6f05223e0c87159c2
Sylar511 <81332605+Sylar511@users.noreply.github.com>
2025-06-08 05:53:46 +0300

    script.js

--- script.js
+++ script.js
@@ -173,7 +173,7 @@ if ('scrollRestoration' in history) {
   history.scrollRestoration = 'manual';
 }
 
-window.addEventListener('load', () => {
+/*window.addEventListener('load', () => {
   window.scrollTo(0, 0);
 });
 
