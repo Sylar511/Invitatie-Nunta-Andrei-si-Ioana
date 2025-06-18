8b1666e7db67d75d0aff2a85345ecb3e15786188
Sylar511 <81332605+Sylar511@users.noreply.github.com>
2025-06-18 10:14:46 +0300

    script.js

--- script.js
+++ script.js
@@ -180,3 +180,17 @@ setTimeout(() => {
     window.scrollTo({ top: 0, behavior: 'instant' });
   }
 }, 100);
+
+const backToTopBtn = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    backToTopBtn.classList.add("show");
+  } else {
+    backToTopBtn.classList.remove("show");
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
